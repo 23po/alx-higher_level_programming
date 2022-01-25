bb9b21ad457d5b0eda74471c74755d799c0c9536
23po <ckodalo@gmail.com>
2022-01-25 07:36:46 -0800

    modified code

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,7 +3,7 @@ def safe_print_division(a, b):
     try:
         result = x // y
     except ZeroDivisionError:
-        print("None")
+        result = None
 
     finally:
-        print("{:d}".format(result))
+        print("Inside result: {}".format(result))

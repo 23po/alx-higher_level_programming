db04359aef67aed184d6444f49e75191c8388832
23po <ckodalo@gmail.com>
2022-01-25 20:17:32 -0800

    changed the thing

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,5 +2,8 @@
 """creating class sqaure again"""
 class Square:
     def __init__(self, size=0):
-        """ defining size attribute of class square"""
+        """
+        Args:
+            size: size of square
+        """
         self.__size = size

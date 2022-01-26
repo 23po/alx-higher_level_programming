ac1397b805ff5f6f3c9c1ef76ce617d7a988e5a9
23po <ckodalo@gmail.com>
2022-01-25 20:27:17 -0800

    no here is where i changed comments

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
-"""creating class sqaure again"""
+"""
+creating class square again
+"""
 class Square:
+    """
+    this is the class
+    """
     def __init__(self, size=0):
         """
         Args:

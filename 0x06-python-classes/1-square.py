a05d73f2c63ae7b3977e2a57405feeb95434751a
23po <ckodalo@gmail.com>
2022-01-25 20:09:12 -0800

    added 1-sqaure.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""creating class sqaure again"""
+class Square:
+    def __init__(self, size=0):
+        """ defining size attribute of class square"""
+        self.__size = size

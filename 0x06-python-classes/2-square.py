30195dc66c61fcf11c975123f8a8971dda3c2c83
23po <ckodalo@gmail.com>
2022-01-25 20:48:23 -0800

    2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+class square:
+    def __init__(self, size=0):
+        if type(size) is no int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+        
+        
+              
